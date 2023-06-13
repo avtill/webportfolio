7ad20877fc33f5fd0b4ea9fc403edb7ad0fe5f69
Joanna <joanna.malgorzata@gmail.com>
2023-06-13 09:21:19 +0200

    main added and color fit nav part and main part

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,14 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import Main from './components/Main/Main';
 
 function App(){
   
   return (
     <div className="App">
       <Navbar />
-      <header className="App-header">
-        <img src="https://avatars.githubusercontent.com/u/114072139?v=4" className="App-logo" alt="profile photo" />
-        <h1>
-          Joanna Mikulińska
-        </h1>
-        <a
-          className="App-link"
-          href="https://github.com/avtill"
-          target="_blank"
-          rel="github link"
-        >
-          Profil na GitHub
-        </a>
-        <a
-          className="App-link"
-          href="https://www.linkedin.com/in/mikulinska/"
-          target="_blank"
-          rel="linkedin link"
-        >
-          Profil na LinkedIn
-        </a>
-      </header>
+      <Main />
     </div>
   );
 }
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "../../App.css";
+
+const Main =()=>{
+    return(
+<header className="App-header">
+        <img src="https://avatars.githubusercontent.com/u/114072139?v=4" className="App-logo" alt="profile photo" />
+        <h1>
+          Joanna Mikulińska
+        </h1>
+        <a
+          className="App-link"
+          href="https://github.com/avtill"
+          target="_blank"
+          rel="github link"
+        >
+          Profil na GitHub
+        </a>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/mikulinska/"
+          target="_blank"
+          rel="linkedin link"
+        >
+          Profil na LinkedIn
+        </a>
+      </header>
+    )
+}
+
+
+export default Main
