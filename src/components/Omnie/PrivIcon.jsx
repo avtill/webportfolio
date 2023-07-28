a0ad10d91a8bdcf1bd61f6362e2b2abbe02cbb59
Joanna <joanna.malgorzata@gmail.com>
2023-07-28 09:16:32 +0200

    added icons to privateIcons component

---
+++ src/components/Omnie/PrivIcon.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../../App.css";
+import gridIcon from "../../img/grid icon.png";
+import textIcon from "../../img/text icon.png"
+
+const PrivIcon =()=> {
+    return (
+        <>
+        <gridIcon />
+        <textIcon />
+        </>
+    )
+}
+
+export default PrivIcon;
---
+++ src/components/Omnie/PrivText.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "../../App.css";
+
+const PrivText = () => {
+    return(
+        <article className="box__private--article">
+        <span className="span__private--tekst">
+            Jestem osobą kreatywną, angażującą się i otwartą. Lubię gry RPG i z dzieckiem poznaję świat Minecrafta. Mam w sercu góry i lasy.
+        </span>
+        <span className="span__private--tekst">
+            Często organizuję: wyjazdy, zajęcia pozalecyjne, przestrzeń wokół mnie. Lubię mieć wszystko uporządkowane i posegregowane. Może dlatego lubię myśleć o świecie na zasadzie schematów? 
+        </span>
+        <span className="span__private--tekst">
+            Niewiele mam wolnego czasu, ale gdy już nie ma ciekawych seriali i filmów fantasy na Netflixie (i innych pokrewnych portalach streamingowych) to chętnie sięgam po kredki lub farby. Ostatnio testuję możliwości akwareli. Lubię też pisać bajki i opowiadania fantasy. Przez kilka lat śpiewałam też w chórze.
+        </span>
+        <span className="span__private--tekst">
+            W najbliższym czasie chciałabym się zaangażować w pracę najbliższego Ogrodu Jordanowskiego w zakresie gier RPG i tematów związanych z fantastyką. 
+        </span>
+    </article>
+    )
+}
+
+export default PrivText;
