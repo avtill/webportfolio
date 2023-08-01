f9816b600ede4ff28e2c803b8f01a70c578fbcbb
Joanna <joanna.malgorzata@gmail.com>
2023-08-01 11:51:15 +0200

    json hobby + gridy & positions

--- src/components/Omnie/PrivGrid.jsx
+++ src/components/Omnie/PrivGrid.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import movieIcon from "../../img/hobby/movie white.png";
 import nutaIcon from "../../img/hobby/nuta white.png";
 import diceIcon from "../../img/hobby/dice white.png";
@@ -6,39 +6,37 @@ import boardIcon from "../../img/hobby/board white.png";
 import gameIcon from "../../img/hobby/game white.png";
 import paintIcon from "../../img/hobby/paint white.png";
 import goryIcon  from "../../img/hobby/gory white.png"
+import hobbyContent from "../../json/hobby.json"
+
 
 const PrivGrid = () => {
+  const [clickedHobbyContent, setClickedHobbyContent] = useState(null);
+
+  const handleHobbyClick = (content) =>{
+    setClickedHobbyContent(content);
+  }
   return (
     <div className='contener--privGrid'>
-      <div className='box__privGrid--empty'></div>
-      <div className='box__privGrid--img'>
-        <img className="img__privGrid--icon" src={movieIcon} alt="ikona filmów"></img>
-      </div>
-      <div className='box__privGrid--empty'></div>
-      <div className='box__privGrid--img'>
-        <img className="img__privGrid--icon" src={nutaIcon} alt="ikona nuty"></img>
-      </div>
-      <div className='box__privGrid--empty'></div>
-      <div className='box__privGrid--img'>
-        <img className="img__privGrid--icon" src={diceIcon} alt="ikona nuty"></img>
-      </div>
-      <div className='box__privGrid--empty'></div>
-      <div className='box__privGrid--img'>
-        <img className="img__privGrid--icon" src={boardIcon} alt="ikona nuty"></img>
-      </div>
-      <div className='box__privGrid--empty'></div>
-      <div className='box__privGrid--img'>
-        <img className="img__privGrid--icon" src={gameIcon} alt="ikona nuty"></img>
-      </div>
-      <div className='box__privGrid--empty'></div>
-      <div className='box__privGrid--img'>
-        <img className="img__privGrid--icon" src={paintIcon} alt="ikona nuty"></img>
-      </div>
-      <div className='box__privGrid--empty'></div>
-      <div className='box__privGrid--img'>
-        <img className="img__privGrid--icon" src={goryIcon} alt="ikona nuty"></img>
-      </div>
-      <div className='box__privGrid--empty'></div>
+      {hobbyContent.map((item,index) =>(
+        <div key={index}
+        className={`box__privGrid--${item.type}`}
+        onClick={() => handleHobbyClick(item.content)}
+        >
+          {item.type==='img' && (
+            <img
+            className='img__privGrid--icon'
+            src={item.icon}
+            alt={`icona ${item.id}`}
+            />
+            )}
+            </div>
+      ))}
+      {clickedHobbyContent && (
+      <div className='tile-content'>
+        <p>{clickedHobbyContent}</p>
+        </div>
+        )
+        }
     </div>
   )
 }
--- src/components/Omnie/PrivIcon.jsx
+++ src/components/Omnie/PrivIcon.jsx
@@ -6,10 +6,10 @@ import textIcon from "../../img/text icon.png"
 const PrivIcon =({setGrid, iconHandlerGrid, iconHandlerText})=> {
 
     return (
-        <>
+        <div className="box__priv--navIcons">
         <img className="img__priv" src={gridIcon} alt="ikona kafelków" onClick={iconHandlerGrid}/>
         <img className="img__priv" src={textIcon} alt="ikona tekstu" onClick={iconHandlerText}/>
-        </>
+        </div>
     )
 }
 
