cf4870f38ae6c7f330bf014cd67949c27dbd32b4
Joanna <joanna.malgorzata@gmail.com>
2023-08-02 17:37:51 +0200

    icons and links - kontakt

--- src/components/Kontakt/Kontakt.jsx
+++ src/components/Kontakt/Kontakt.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "../../App.css";
+import github from "../../img/github.png";
+import linkedin from "../../img/linkedin.png";
+import mail from "../../img/Mail.png"
 
 const Kontakt =()=>{
     return(
@@ -7,6 +10,24 @@ const Kontakt =()=>{
             <h2>
                 Kontakt
             </h2>
+            <a href="https://github.com/avtill" target="_blank">
+                <div className="box__contact--items">
+                <img className="icon--contact" src={github} alt="ikona github" />
+                <p>GitHub</p>
+                </div>
+            </a>
+            <a href="https://www.linkedin.com/in/mikulinska/" target="_blank">
+                <div className="box__contact--items">
+                <img className="icon--contact" src={linkedin} alt="ikona LinkedIn"/>
+                <p>LinkedIn</p>
+                </div>
+            </a>
+            <a href="mailto:joanna.malgorzata@gmail.com" target="_blank">
+                <div className="box__contact--items">   
+                <img className="icon--contact" src={mail} alt="ikona poczty"/>
+                <p>Mail</p>
+            </div>
+            </a>
         </section>
     )
 }
