bfcc69e11a0b065be56c51ea3390e9f0ea0c0668
Joanna <joanna.malgorzata@gmail.com>
2023-08-07 14:14:39 +0200

    tekst elementów portfolio + hidden

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -14,34 +14,74 @@ const Portfolio=()=>{
                         <img className="portfolio--miniature" src={tracker} alt="miniatura aplikacji time tracker"></img>
                     </a>
                     <div className="box__portfolio--techText">
-                       <div> <ul className="portfolio__list">
-                            <li>JavaScript</li>
-                            <li>HTML</li>
-                            <li>CSS
-                                <ul>
-                                    <li>RWD</li>
-                                    <li>grid</li>
-                                </ul>
-                            </li>
-                            <li>
-                                JSON
-                            </li>
-                        </ul>
+                        <div>
+                            <ul className="portfolio__list">
+                                <li>JavaScript</li>
+                                <li>HTML</li>
+                                <li>CSS
+                                    <ul>
+                                        <li>RWD</li>
+                                        <li>grid</li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    JSON
+                                </li>
+                            </ul>
                         </div>
                         <div className="box__portfolio--describeText">
                             <p className="portfolio__paragraph--text">
-                                Pomysł na projekt zrealizowany ze strony FrontendMentor. Wykonałam go na podstawie dostarczonych szablonów korzystając przede wszystkim z grida oraz właściwości "position". Zachowałam też wygląd mobilny i zadbałam o responsywność strony. Były to moje pierwsze próby korzystania z informacji zaciąganym z plików JSON w projektach. 
+                                    Pomysł na projekt zrealizowany ze strony FrontendMentor. Wykonałam go na podstawie dostarczonych szablonów korzystając przede wszystkim z grida oraz właściwości "position". Zachowałam też wygląd mobilny i zadbałam o responsywność strony. Były to moje pierwsze próby korzystania z informacji zaciąganym z plików JSON w projektach. 
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="box__portfolio--content">
+                    <a href="https://github.com/sabiku85/easybank-lp" target="_blank">
+                        <img className="portfolio--miniature" src={bank} alt="miniatura strony banku"></img>
+                    </a>
+                    <div className="box__portfolio--techText">
+                        <div>
+                            <ul className="portfolio__list">
+                                <li>JavaScript</li>
+                                <li>HTML</li>
+                                <li>SASS
+                                    <ul>
+                                        <li>RWD</li>
+                                        <li>grid</li>
+                                        <li>flexbox</li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="box__portfolio--describeText">
+                            <p className="portfolio__paragraph--text">
+                                Pomysł na projekt zrealizowany ze strony FrontendMentor w małej dwuosobowej grupie w celu przećwiczenia umiejętności twardych oraz miękkich. Dzięki współpracy z innym programistą ugruntowałam sobie wiedzę z GITa oraz nauczyłam się korzystać z SASSa. 
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="box__portfolio--content">
+                    <a href="https://github.com/avtill/rolladice" target="_blank">  
+                        <img className="portfolio--miniature" src={dice} alt="miniatura aplikacji Roll Dice"></img>
+                    </a>
+                    <div className="box__portfolio--techText">
+                        <div>
+                            <ul className="portfolio__list">
+                                <li>JavaScript</li>
+                                <li>TypeScript</li>
+                                <li>React</li>
+                                <li>HTML</li>
+                                <li>CSS (RWD)</li>
+                                <li>MUI</li>
+                            </ul>
+                        </div>
+                        <div className="box__portfolio--describeText">
+                            <p className="portfolio__paragraph--text">
+                                To pierwszy taki pomysł na aplikację, który miałam i który chcę doszlifować dość solidnie. Póki co jest to bardzo prosty algorytm do wyliczania wieloktornych rzutów nietypowymi kośćmi (potrzebny w grach typu RPG). Chciałabym rozwinąć podawanie wyników oraz możliwość zapisu wyników po logowaniu. Mam wrażenie, że sporo chciałabym osiągnąć tą aplikacją dlatego zrobiłam jej podstawową funkcjonalność na ten czas i odstawiłam projekt na półkę. Chciałam się skupić na podszlifowaniu umiejętności nim wrócę i zagłębię się w funkcjonalności, na których mi najbardziej zależy.
                             </p>
                         </div>
                         </div>
-                </div>
-                <div className="box__portfolio--content">
-                    <img className="portfolio--miniature" src={bank} alt="miniatura strony banku"></img>
-                    <p className="portfolio__paragraph--text">Bank langing page - opis projektu i technologii</p>
-                </div>
-                <div className="box__portfolio--content">
-                    <img className="portfolio--miniature" src={dice} alt="miniatura aplikacji Roll Dice"></img>
-                    <p className="portfolio__paragraph--text">Roll Dice alikacja - stack technologiczny + opisik projektu</p>
                 </div>
             </div>
         </section>
